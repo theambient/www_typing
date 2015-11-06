6d4068d5099ce6132678c7d7d43240c952b311ed
Ruslan Mullakhmetov <ruslan@Ruslans-MacBook-Pro.local>
2015-11-06 18:40:35 +0300

    initaial

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/stathisg/pen/Bkvhg.
+
+ Demo of typing & erasing effects, plus a blinking cursor ([full-text tutorial](http://burnmind.com/tutorials/how-to-create-a-typing-effect-an-eraser-effect-and-a-blinking-cursor-using-jquery)).
---
+++ js/index.js
@@ -0,0 +1,52 @@
+var captionLength = 0;
+var caption = '';
+var erase_len = 70;
+var type_again_len = 57;
+var erase_flag = true;
+var caption = "Productivity is my passion. Helping people get things done is my calling. Making you and your team more productive already in the first days you use ABC is my promise.<br><br>I personally guarantee you'll be more productive 30 days from now.<br><br>When you sign up for our ABC trial, not only will you get access to a productivity system designed for all the major platforms, that's been helping 300,000+ busy professionals get things done in the last 7+ years.<br>.<br>I will also personally teach you all the best productivity principles I know through video tutorials, my popular \"10-Steps to Ultimate Productivity\" course and my \"Productive! Magazine\". My goal is to help you achieve greatness while living a more balanced and better organized life.<br><br>My Signature<br><br>Mudila Strashniy<br><br>Productivity guy, founder and CEO of ABC";
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+    captionEl = $('#caption');
+
+    testTypingEffect();
+
+});
+
+function testTypingEffect() {
+
+    type();
+}
+
+function type() {
+    captionEl.html(caption.substr(0, captionLength++));
+    if(captionLength < caption.length+1) {
+        if(erase_flag && captionLength >= erase_len)
+        {
+            setTimeout('erase()', 500);
+            erase_flag = false;
+        }
+        else
+            setTimeout('type()', 50);
+    } else {
+        captionLength = 0;
+        caption = '';
+    }
+}
+
+function erase() {
+    captionEl.html(caption.substr(0, captionLength--));
+    if(captionLength >= type_again_len) {
+        setTimeout('erase()', 50);
+    } else {
+        setTimeout('type()', 300);
+    }
+}
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
